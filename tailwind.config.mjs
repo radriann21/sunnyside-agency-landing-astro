a23c5a278b7ce9bc7e69da03c591ff87dc9ae4d2
radriann21 <adrianrangelargu@gmail.com>
2025-01-21 18:57:30 -0400

    add footer, hero, navbar, gallery, and testimonial components; update layout and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,36 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					'soft-red': 'hsl(7, 99%, 70%)',
+					'custom-yellow': 'hsl(51, 100%, 49%)',
+					'dark-desaturated-cyan': 'hsl(167, 40%, 24%)',
+					'dark-blue': 'hsl(198, 62%, 26%)',
+					'dark-moderate-cyan': 'hsl(168, 34%, 41%)'
+				},
+				neutral: {
+					'very-dark-desaturated-blue': 'hsl(212, 27%, 19%)',
+					'very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
+					'dark-grayish-blue': 'hsl(232, 10%, 55%)',
+					'grayish-blue': 'hsl(210, 4%, 67%)'
+				}
+			},
+			fontFamily: {
+				'headings': 'Fraunces',
+				'body': 'Barlow'
+			},
+			backgroundImage: {
+				'hero-desktop': 'url("/assets/desktop/image-header.jpg")',
+				'hero-mobile': 'url("/assets/mobile/image-header.jpg")',
+				'photography-desktop': 'url("/assets/desktop/image-photography.jpg")',
+				'photography-mobile': 'url("/assets/mobile/image-photography.jpg")',
+			}
+		},
 	},
 	plugins: [require('daisyui')],
+	daisyui: {
+		darkTheme: 'light'
+	}
 }
